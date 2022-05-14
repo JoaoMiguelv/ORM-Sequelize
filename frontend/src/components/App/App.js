cdeca81bd381d3ae54a15f746920b1870ed44f23
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2022-05-13 22:17:51 -0300

    frontend-react

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Iniciando com ReactJs</h2>
+    </div>
+  );
+}
+
+export default App;
